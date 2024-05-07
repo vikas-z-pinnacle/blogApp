0b801a057515551b18a2fc5d42612150b66aea6a
Vikas <vikas.zilpe@pinnacle.in>
2024-05-07 12:07:13 +0530

    Initial project setup

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
+mongoose.
+    connect(process.env.MONGODB)
+    .then(() => {
+        console.log("Connected to db");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
