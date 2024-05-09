bab472d6ae8395e0c6cabd9c7fa5a2c63fa45a67
Vikas <vikas.zilpe@pinnacle.in>
2024-05-09 14:47:04 +0530

    Signin API

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/errorHandler.js";
 import User from "./../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -23,4 +24,43 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+}
+
+export const signin = async (req, res, next) => {
+    const { username, password } = req.body;
+
+    if (!username || !password || username === '' || password === '') {
+        next(errorHandler(400, "All fields are required."));
+    }
+
+    try {
+        let user;
+        const isEmail = validateEmail(username);
+        if (isEmail) {
+            user = await User.findOne({ email: username });
+        } else {
+            user = await User.findOne({ username: username });
+        }
+        if (!user) {
+            return next(errorHandler(400, "Please provide valide details"));
+        }
+
+        const validPass = bcryptjs.compareSync(password, user.password);
+        if (!validPass) {
+            return next(errorHandler(400, "Please provide valide details"));
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+        const { password: pass, ...rest } = user._doc;
+
+        res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
